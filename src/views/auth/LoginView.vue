a7404af9597bc0ae24339114cbbca056a411c679
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-05-08 20:11:36 -0400

    Define Routes and User Registration and Authentication Layout

---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Desde LoginView</h1>
+    </div>
+</template>
+
---
+++ src/views/auth/RegisterView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Desde RegisterView</h1>
+    </div>
+</template>
+
