ea54b9815c99c1974c7b542158dfdba5a4123c67
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-05-08 20:44:18 -0400

    Theming to FormKit forms

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -6,10 +6,24 @@
     <h1 class="text-6xl font-extrabold text-white text-center mt-10">Crea una cuenta</h1>
     <h1 class="text-2xl text-white text-center my-5">Crea una cuenta en AppSalon</h1>
 
-    <FormKit>
+    <FormKit
+        type="form"
+        :actions="false"
+        incomplete-message="No se pudo enviar, revisa las notificaciones"
+    >
         <FormKit
             type="text"
+            label="Nombre"
+            name="name"
+            placeholder="Tu Nombre"
+            validation="required|length:3"
+            :validation-messages="{
+                required: 'El Nombre es obligatorio',
+                length: 'El Nombre es muy corto',
+            }"
         />
+
+        <FormKit type="submit">Crear Cuenta</FormKit>
     </FormKit>
 </template>
 
