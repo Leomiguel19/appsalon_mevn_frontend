9b918008ed98dcb5e1d083c41f7b37d4c66b2e6d
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-05-12 22:54:48 -0400

    Create My Appointments Page

---
+++ src/views/appointments/MyAppointments.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <h2 class="text-4xl font-extrabold text-white">Mis Citas</h2>
+    <p class="text-white text-lg mt-5">A continuacion pódras administrar tus próximas citas</p>
+
+</template>
+
