653f209c7159544f8355c91b8cf4c4b145d36a0f
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-05-22 21:55:05 -0400

    First steps to show Appointment details

---
+++ src/components/Appointment.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { displayDate } from '@/helpers/date';
+
+    defineProps({
+        appointment: {
+            type: Object
+        }
+    })
+</script>
+
+<template>
+    <div class="bg-white p-5 space-y-3 rounded-lg">
+        <p class="text-gray-500 font-black">
+            Fecha: <span class="font-light"> {{displayDate(appointment.date)}} </span>
+            Hora: <span class="font-light"> {{ appointment.time }} </span>
+        </p>
+    </div>
+</template>
--- src/views/appointments/MyAppointments.vue
+++ src/views/appointments/MyAppointments.vue
@@ -1,6 +1,7 @@
 <script setup>
+    import Appointment from "@/components/Appointment.vue"
     import { useUserStore } from '@/stores/user'
-    const user = useUserStore
+    const user = useUserStore()
 </script>
 
 <template>
@@ -11,6 +12,14 @@
 
     <div v-else>
         <p v-if="user.noAppointments" class="text-white text-2xl text-center mt-5">No tienes proximas citas</p>
+
+        <div class="grid grid-cols-1 gap-5 mt-10">
+            <Appointment
+                v-for="appointment in user.userAppointments"
+                :key="appointment._id"
+                :appointment="appointment"
+            />
+        </div>
     </div>
 </template>
 
